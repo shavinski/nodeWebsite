f3ecb514656b0af1fb9af42191052acb2ec123fb
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-22 19:04:00 -0700

    remove some files like routes
    Finsih up server.js file and serves html depending on endpoint

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const { createServer } = require("node:http");
-const fetchDataFromServer = require("./clientRequest");
+const fs = require("fs");
+const path = require("path");
+
+const { url } = require("node:inspector");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8080;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World Test");
+  let filePath = "." + req.url;
+  console.log("url =>", req.url);
+
+  if (filePath === "./") {
+    filePath = "./index.html";
+  }
+
+  const urlMapping = {
+    "/": "./index.html",
+    "/about": "./about.html",
+    "/contact-me": "./contact-me.html",
+    "/404": "./404.html",
+  };
+
+  filePath = urlMapping[req.url] || "./404.html";
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      filePath = "./404.html";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Internal Server Error, Oopsies");
+        return;
+      }
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "plain-text");
+      res.end(data);
+    });
+  });
 });
 
 server.listen(port, hostname, () => {
