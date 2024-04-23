b55a2a953366f318d99219835017ec70537a2af3
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-22 18:40:46 -0700

    Add h1's to all html files'

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const { createServer } = require('node:http');
+const { createServer } = require("node:http");
+const fetchDataFromServer = require("./clientRequest");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World Test');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World Test");
 });
 
 server.listen(port, hostname, () => {
