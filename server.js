69b783eaf620b3badf55eb1b74604082d769f3c1
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-25 18:22:26 -0700

    follow lesson

--- server.js
+++ server.js
@@ -1,49 +1,19 @@
-const { createServer } = require("node:http");
-const fs = require("fs");
-const path = require("path");
-
-const { url } = require("node:inspector");
+// Load HTTP module
+const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = 8080;
+const port = 8000;
 
-const server = createServer((req, res) => {
-  let filePath = "." + req.url;
-  console.log("url =>", req.url);
+// Create HTTP server
+const server = http.createServer(function (req, res) {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { "Content-Type": "text/plain" });
 
-  if (filePath === "./") {
-    filePath = "./index.html";
-  }
-
-  const urlMapping = {
-    "/": "./index.html",
-    "/about": "./about.html",
-    "/contact-me": "./contact-me.html",
-    "/404": "./404.html",
-  };
-
-  filePath = urlMapping[req.url] || "./404.html";
-
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      filePath = "./404.html";
-    }
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Internal Server Error, Oopsies");
-        return;
-      }
-
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "plain-text");
-      res.end(data);
-    });
-  });
+  // Send the response body "Hello World"
+  res.end("Hello World\n");
 });
 
-server.listen(port, hostname, () => {
+// Prints a log once the server starts listening
+server.listen(port, hostname, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
