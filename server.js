a5061a3aab7262d6e8396778fda6c854f9dd04a0
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-29 18:31:46 -0700

    Utilize express and create server using node and express
    Download express dependencies

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // Load HTTP module
-const http = require("http");
+const express = require("express");
+const app = express();
+const createError = require("http-errors");
 
-const hostname = "127.0.0.1";
-const port = 8000;
+const hostname = "localhost";
+const port = 3000;
 
-// Create HTTP server
-const server = http.createServer(function (req, res) {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { "Content-Type": "text/plain" });
+const indexRouter = require("./routes/index");
 
-  // Send the response body "Hello World"
-  res.end("Hello World\n");
+app.use("/", indexRouter);
+
+app.listen(port, function () {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
-// Prints a log once the server starts listening
-server.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+module.exports = app;
